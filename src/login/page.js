8777685af1d75144753509b360f554004199fc7a
David Ignacio Abril Perrig <dabrilperrig@ubp.edu.ar>
2026-01-14 15:56:21 -0300

    Fix syntax error in login page and add babel plugin

--- src/login/page.js
+++ src/login/page.js
@@ -1,4 +1,4 @@
-"import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, Checkbox, Typography } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { Link, useLocation } from "wouter";
